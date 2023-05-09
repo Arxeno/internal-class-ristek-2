93bc7bee5ffb44e73008dc2a1ba1d6fc37edcfb3
Arxeno <rmfbkw04@gmail.com>
2023-05-10 00:45:37 +0700

    add dummy data

---
+++ server/data.ts
@@ -0,0 +1,228 @@
+export const expensesData = [
+  {
+    id: 'e88e57c6-9bd1-485b-8cba-3e9645c64b8d',
+    amount: 186,
+    created_at: '2023-03-24T00:00:00.000Z',
+    category: {
+      name: 'Food',
+    },
+  },
+  {
+    id: '40ea3a70-6282-468f-9fd5-2eef4a92e704',
+    amount: 144,
+    created_at: '2023-03-22T00:00:00.000Z',
+    category: {
+      name: 'Food',
+    },
+  },
+  {
+    id: '19019f60-12ae-46ea-a7d2-8e6e74b36210',
+    amount: 115,
+    created_at: '2023-03-21T00:00:00.000Z',
+    category: {
+      name: 'Transportation',
+    },
+  },
+  {
+    id: '654ba665-a200-4c88-8d86-34ed428a494a',
+    amount: 128,
+    created_at: '2023-03-20T00:00:00.000Z',
+    category: {
+      name: 'Transportation',
+    },
+  },
+  {
+    id: '1cc6910c-2199-4161-9cc2-4afbce992c62',
+    amount: 189,
+    created_at: '2023-03-16T00:00:00.000Z',
+    category: {
+      name: 'Food',
+    },
+  },
+  {
+    id: '2c056901-505d-4fb4-9bba-55aee6614f5f',
+    amount: 4746,
+    created_at: '2023-03-13T00:00:00.000Z',
+    category: {
+      name: 'Housing',
+    },
+  },
+  {
+    id: '6a1f0505-a285-4481-92df-ffca07bbf76e',
+    amount: 176,
+    created_at: '2023-03-10T00:00:00.000Z',
+    category: {
+      name: 'Transportation',
+    },
+  },
+  {
+    id: '074d5e52-b5fb-4e12-8ccf-b1ae77c5728a',
+    amount: 152,
+    created_at: '2023-02-18T00:00:00.000Z',
+    category: {
+      name: 'Food',
+    },
+  },
+  {
+    id: 'dacd7096-72e7-49fe-9271-84bf89ffa798',
+    amount: 147,
+    created_at: '2023-02-17T00:00:00.000Z',
+    category: {
+      name: 'Food',
+    },
+  },
+  {
+    id: '8b56600f-13c2-4faf-9604-35f3c513cfcb',
+    amount: 3086,
+    created_at: '2023-02-12T00:00:00.000Z',
+    category: {
+      name: 'Housing',
+    },
+  },
+];
+
+export const categoriesId = [
+  {
+    id: 'd8e6963a-b544-4c31-bc90-6bb3e15203e2',
+    name: 'Transportation',
+  },
+  {
+    id: 'f12399a7-302c-452a-89d8-5ec21c4514e8',
+    name: 'Food',
+  },
+  {
+    id: 'afc106af-2790-4df0-8ed2-473d6ef4b595',
+    name: 'Housing',
+  },
+  {
+    id: '6bcd7235-717e-43b9-bed1-13e0b04e4c0b',
+    name: 'Personal Spending',
+  },
+];
+
+export const currentExpense = {
+  total: '344638',
+};
+
+export const expensesDetail = [
+  {
+    id: 'e88e57c6-9bd1-485b-8cba-3e9645c64b8d',
+    name: 'BBQ or Grilling Event',
+    amount: 186,
+    description:
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.',
+    created_at: '2023-03-24T00:00:00.000Z',
+    category: {
+      id: 'f12399a7-302c-452a-89d8-5ec21c4514e8',
+      name: 'Food',
+    },
+  },
+  {
+    id: '40ea3a70-6282-468f-9fd5-2eef4a92e704',
+    name: 'Meal Prep',
+    amount: 144,
+    description:
+      'Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    created_at: '2023-03-22T00:00:00.000Z',
+    category: {
+      id: 'f12399a7-302c-452a-89d8-5ec21c4514e8',
+      name: 'Food',
+    },
+  },
+  {
+    id: '19019f60-12ae-46ea-a7d2-8e6e74b36210',
+    name: 'Motorcycle Rental',
+    amount: 115,
+    description:
+      'Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros.',
+    created_at: '2023-03-21T00:00:00.000Z',
+    category: {
+      id: 'd8e6963a-b544-4c31-bc90-6bb3e15203e2',
+      name: 'Transportation',
+    },
+  },
+  {
+    id: '654ba665-a200-4c88-8d86-34ed428a494a',
+    name: 'Train Ride',
+    amount: 128,
+    description:
+      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum.',
+    created_at: '2023-03-20T00:00:00.000Z',
+    category: {
+      id: 'd8e6963a-b544-4c31-bc90-6bb3e15203e2',
+      name: 'Transportation',
+    },
+  },
+  {
+    id: '1cc6910c-2199-4161-9cc2-4afbce992c62',
+    name: 'BBQ or Grilling Event',
+    amount: 189,
+    description:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+    created_at: '2023-03-16T00:00:00.000Z',
+    category: {
+      id: 'f12399a7-302c-452a-89d8-5ec21c4514e8',
+      name: 'Food',
+    },
+  },
+  {
+    id: '2c056901-505d-4fb4-9bba-55aee6614f5f',
+    name: 'Property Sale',
+    amount: 4746,
+    description:
+      'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices.',
+    created_at: '2023-03-13T00:00:00.000Z',
+    category: {
+      id: 'afc106af-2790-4df0-8ed2-473d6ef4b595',
+      name: 'Housing',
+    },
+  },
+  {
+    id: '6a1f0505-a285-4481-92df-ffca07bbf76e',
+    name: 'Segway Tour',
+    amount: 176,
+    description:
+      'Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis.',
+    created_at: '2023-03-10T00:00:00.000Z',
+    category: {
+      id: 'd8e6963a-b544-4c31-bc90-6bb3e15203e2',
+      name: 'Transportation',
+    },
+  },
+  {
+    id: '074d5e52-b5fb-4e12-8ccf-b1ae77c5728a',
+    name: 'BBQ or Grilling Event',
+    amount: 152,
+    description:
+      'Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.',
+    created_at: '2023-02-18T00:00:00.000Z',
+    category: {
+      id: 'f12399a7-302c-452a-89d8-5ec21c4514e8',
+      name: 'Food',
+    },
+  },
+  {
+    id: 'dacd7096-72e7-49fe-9271-84bf89ffa798',
+    name: 'Wine Pairing',
+    amount: 147,
+    description:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum.',
+    created_at: '2023-02-17T00:00:00.000Z',
+    category: {
+      id: 'f12399a7-302c-452a-89d8-5ec21c4514e8',
+      name: 'Food',
+    },
+  },
+  {
+    id: '8b56600f-13c2-4faf-9604-35f3c513cfcb',
+    name: 'Property Rent',
+    amount: 3086,
+    description:
+      'Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum.',
+    created_at: '2023-02-12T00:00:00.000Z',
+    category: {
+      id: 'afc106af-2790-4df0-8ed2-473d6ef4b595',
+      name: 'Housing',
+    },
+  },
+];
