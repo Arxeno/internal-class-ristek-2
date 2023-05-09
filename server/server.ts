be32fd490fac22953fab73535d1bae438751a69d
Arxeno <rmfbkw04@gmail.com>
2023-05-10 02:52:42 +0700

    finish post /expense

--- server/server.ts
+++ server/server.ts
@@ -13,6 +13,22 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+// helpers
+const makeId = (length: number) => {
+  let result = '';
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+
+app.use(express.json());
+
+// GET
 app.get('/expense', (req: Request<string | number>, res: Response) => {
   const { category_id } = req.query;
   let { min_price, max_price, page, limit } = req.query;
@@ -98,7 +114,7 @@ app.get('/expense', (req: Request<string | number>, res: Response) => {
   let itemCount = 0;
 
   for (let i = 0; i < expensesData.length; i++) {
-    if (expensesData[i].category.name == categoryName || categoryName == '') {
+    if (expensesData[i].category == categoryName || categoryName == '') {
       if (minPriceNumber == -1 || expensesData[i].amount >= minPriceNumber) {
         if (maxPriceNumber == -1 || expensesData[i].amount <= maxPriceNumber) {
           dataFiltered.push(expensesData[i]);
@@ -160,6 +176,48 @@ app.get('/expense/:id', (req: Request, res: Response) => {
     statusCode: 500,
     message: 'Internal server error',
   });
+});
+
+app.post('/expense', (req: Request, res: Response) => {
+  const { body } = req;
+
+  const newId = `${makeId(8)}-${makeId(4)}-${makeId(4)}-${makeId(4)}-${makeId(
+    12
+  )}`;
+
+  let categoryName = '';
+  for (const categoryId of categoriesId) {
+    if (categoryId.id == body.category) {
+      categoryName = categoryId.name;
+    }
+  }
+
+  const newExpenseData = {
+    id: newId,
+    name: body.name,
+    category: categoryName,
+    amount: body.amount,
+  };
+
+  expensesData.push(newExpenseData);
+
+  const newExpenseDetail = {
+    id: newId,
+    name: body.name,
+    category: {
+      id: body.category,
+      name: categoryName,
+    },
+    amount: body.amount,
+  };
+
+  expensesDetail.push(newExpenseDetail);
+
+  res.json(newExpenseDetail);
+
+  console.log(expensesData);
+  console.log('=============================================');
+  console.log(expensesDetail);
 });
 
 app.listen(port, () => {
